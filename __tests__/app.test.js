552b46825a14482b57ca9c5c513a7630eb1fcddb
Ben <benjamurph@hotmail.co.uk>
2022-07-05 10:10:52 +0100

    finished 5-PATCH/api/reviews/:review_id, this allows for adding or subtracting votes from a review based on its review id

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -12,7 +12,7 @@ afterAll(() => {
     return db.end();
 });
 
-describe('api/categories', () => {
+describe('GET api/categories', () => {
     describe('happy path', () => {
         test('200 status: returns an array containing all the categories', () => {
             return request(app)
@@ -39,7 +39,7 @@ describe('api/categories', () => {
   });   
 });
 
-describe('api/reviews/:review_id', () => {
+describe('GET api/reviews/:review_id', () => {
     describe('happy paths', () => {
         test('200 status: returns the review that corresponds to the input review_id', () => {
             return request(app)
@@ -56,7 +56,7 @@ describe('api/reviews/:review_id', () => {
                   review_img_url: 'https://www.golenbock.com/wp-content/uploads/2015/01/placeholder-user.png',
                   created_at: 'Mon Jan 18 2021 10:00:20 GMT+0000 (Greenwich Mean Time)',
                   votes: 1
-                });;
+                });
             });
           });
     });
@@ -75,6 +75,96 @@ describe('api/reviews/:review_id', () => {
             .expect(400)
             .then(({body}) => {
                 expect(body.msg).toBe('bad request');
+            });
+        });
+    });
+});
+
+describe('PATCH api/reviews/:review_id', () => {
+    describe('happy path', () => {
+        test('PATCH 200: updates an existing review to increase or decrease the review/s votes by the given amount', () => {
+          const newVote = {
+            inc_votes : 1
+          }
+          return request(app)
+          .patch('/api/reviews/1')
+          .send(newVote)
+          .expect(200)
+          .then(({body}) => {
+            expect(body.review).toEqual({
+                review_id: 1,
+                title: 'Agricola',
+                category: 'euro game',
+                designer: 'Uwe Rosenberg',
+                owner: 'mallionaire',
+                review_body: 'Farmyard fun!',
+                review_img_url: 'https://www.golenbock.com/wp-content/uploads/2015/01/placeholder-user.png',
+                created_at: 'Mon Jan 18 2021 10:00:20 GMT+0000 (Greenwich Mean Time)',
+                votes: 2
+              });
+          });
+        });
+    });
+    describe('error handling', () => {
+        test('404 status: returns the message "400 not found, please input a valid path." when presented with a path that doesn\'t exist', () => {
+            const newVote = {
+                inc_votes : 1
+              }
+            return request(app)
+          .patch('/api/reviews/999')
+          .send(newVote)
+          .expect(404)
+          .then(({body}) => {
+            expect(body.msg).toBe('no review found under id 999');
+          });
+        });  
+        test('400 status: returns a bad path request', () => {
+            const newVote = {
+                inc_votes : 1
+              }
+            return request(app)
+            .patch('/api/reviews/notanumber')
+            .send(newVote)
+            .expect(400)
+            .then(({body}) => {
+                expect(body.msg).toBe('bad request');
+            });
+        });
+        test('400 status: returns the message "Invalid patch request, please reformat your patch" when the patch does not contain "inc_votes"', () => {
+            const newVote = {
+                name: 'Mitch'
+              }
+              return request(app)
+            .patch('/api/reviews/1')
+            .send(newVote)
+            .expect(400)
+            .then(({body}) => {
+                expect(body.msg).toBe('Invalid patch request, please reformat your patch');
+            });
+        });
+        test('400 status: returns the message "Invalid patch request, please reformat your patch" when the patch contains extra properties', () => {
+            const newVote = {
+                inc_votes: 1,
+                name: 'Mitch'
+              }
+              return request(app)
+            .patch('/api/reviews/1')
+            .send(newVote)
+            .expect(400)
+            .then(({body}) => {
+                expect(body.msg).toBe('Invalid patch request, please reformat your patch');
+            });
+        });
+        test('400 status: returns the message "Invalid patch request, please reformat your patch" when vote_count is of the wrong type', () => {
+            const newVote = {
+                inc_votes: 'cat',
+              }
+              return request(app)
+            .patch('/api/reviews/1')
+            .send(newVote)
+            .expect(400)
+            .then(({body}) => {
+                expect(body.msg).toBe('Invalid patch request, please reformat your patch');
             });
         });
     });
--- controllers/games.js
+++ controllers/games.js
@@ -1,10 +1,11 @@
 const { selectCategories,
-        selectReviewById
+        selectReviewById,
+        updateVotes
       } = require('../models/games');
 
 exports.getCategories = (req, res, next) => {
     selectCategories().then((categories) => {
-        res.status(200).send({ categories })
+        res.status(200).send({ categories });
     })
     .catch((err) => {
         next(err);
@@ -13,8 +14,16 @@ exports.getCategories = (req, res, next) => {
 
 exports.getReviewById = (req, res, next) => {
     selectReviewById(req.params).then((review) => {
-        review.created_at = `${review.created_at}`
-        res.status(200).send({ review })
+        review.created_at = `${review.created_at}`;
+        res.status(200).send({ review });
+    })
+    .catch(next);
+};
+
+exports.updateReview = (req, res, next) => {
+    updateVotes(req.body, req.params).then((review) => {
+        review.created_at = `${review.created_at}`;
+        res.status(200).send({ review });
     })
     .catch(next);
 };
